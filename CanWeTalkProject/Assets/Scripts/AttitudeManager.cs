b37968bd482c1ca6ac24fde0eaf34980f96d2cf7
StephanieGardiner <s210449@students.aie.edu.au>
2023-05-02 15:10:36 +0930

    added vcr font, added attitude manager

---
+++ CanWeTalkProject/Assets/Scripts/AttitudeManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttitudeManager : MonoBehaviour
+{
+
+    public int wifeAttitude;
+    public int youAttitude;
+    public int childrenAttitude;
+    public int dogAttitude;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
