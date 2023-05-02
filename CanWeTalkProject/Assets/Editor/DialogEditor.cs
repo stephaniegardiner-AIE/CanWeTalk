531052ae6ede6f54fdd63c633fbef5da6d884ecb
StephanieGardiner <s210449@students.aie.edu.au>
2023-05-02 16:55:54 +0930

    doing me best :)

--- CanWeTalkProject/Assets/Editor/DialogEditor.cs
+++ CanWeTalkProject/Assets/Editor/DialogEditor.cs
@@ -5,10 +5,12 @@ using System.Collections.Generic;
 
 public class DialogEditor : EditorWindow
 {
-    string myString = "Hello World";
-    bool groupEnabled;
-    bool myBool = true;
-    float myFloat = 1.23f;
+    List<Rect> windows = new List<Rect>();
+    List<int> windowsToAttach = new List<int>();
+    List<int> attachedWindows = new List<int>();
+
+    //int attitudes = 1;
+    
 
     // Add menu item named "My Window" to the Window menu
     [MenuItem("Window/Dialog Editor Window")]
@@ -20,12 +22,77 @@ public class DialogEditor : EditorWindow
 
     void OnGUI()
     {
-        GUILayout.Label("Base Settings", EditorStyles.boldLabel);
-        myString = EditorGUILayout.TextField("Text Field", myString);
+        
 
-        groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
-        myBool = EditorGUILayout.Toggle("Toggle", myBool);
-        myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
-        EditorGUILayout.EndToggleGroup();
+        if (windowsToAttach.Count == 2)
+        {
+            attachedWindows.Add(windowsToAttach[0]);
+            attachedWindows.Add(windowsToAttach[1]);
+            windowsToAttach = new List<int>();
+        }
+
+        if (attachedWindows.Count >= 2)
+        {
+            for (int i = 0; i < attachedWindows.Count; i += 2)
+            {
+                //DrawNodeCurve(windows[attachedWindows[i]], windows[attachedWindows[i + 1]]);
+            }
+        }
+
+        BeginWindows();
+
+        if (GUILayout.Button("Create Node"))
+        {
+            windows.Add(new Rect(10, 10, 200, 200));
+        }
+
+        for (int i = 0; i < windows.Count; i++)
+        {
+            windows[i] = GUI.Window(i, windows[i], DrawNodeWindow, "Window " + i);
+        }
+
+
+        
+        if (GUILayout.Button("Clear Nodes"))
+        {
+            windows.Clear(); 
+        }
+
+
+        EndWindows();
     }
+
+
+    void DrawNodeWindow(int id)
+    {
+        if (GUILayout.Button("Attach"))
+        {
+            windowsToAttach.Add(id);
+        }
+
+        //int attitudeChange;
+        int attitudeChange = EditorGUILayout.IntField("Attitude Change Amount:", 0);
+
+        GUI.DragWindow();
+    }
+
+
+
+
+   /* void DrawNodeCurve(Rect start, Rect end)
+    {
+        Vector3 startPos = new Vector3(start.x + start.width, start.y + start.height / 2, 0);
+        Vector3 endPos = new Vector3(end.x, end.y + end.height / 2, 0);
+        Vector3 startTan = startPos + Vector3.right * 50;
+        Vector3 endTan = endPos + Vector3.left * 50;
+        Color shadowCol = new Color(0, 0, 0, 0.06f);
+
+        for (int i = 0; i < 3; i++)
+        {// Draw a shadow
+            Handles.DrawBezier(startPos, endPos, startTan, endTan, shadowCol, null, (i + 1) * 5);
+        }
+
+        Handles.DrawBezier(startPos, endPos, startTan, endTan, Color.black, null, 1);
+    } */
 }
+
