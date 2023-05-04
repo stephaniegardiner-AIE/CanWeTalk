0943d0356e489b2d54c5f799d290f16e549a4c58
DELLA\steph <s210449@students.aie.edu.au>
2023-05-04 19:50:28 +0930

    trying to get typewriter effect going

--- CanWeTalkProject/Assets/Scripts/SceneStarter.cs
+++ CanWeTalkProject/Assets/Scripts/SceneStarter.cs
@@ -13,6 +13,9 @@ public class SceneStarter : MonoBehaviour
     public float yPositionSpeechBubble;
     public GameObject dialogText;
     public float lineNumber;
+    public TextMeshProUGUI currentTextToWrite;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -35,10 +38,61 @@ public class SceneStarter : MonoBehaviour
 
         dialogText = GameObject.Find("CharacterDialogText");
 
-        dialogText.GetComponent<TextMeshProUGUI>().text = startLineBlock.sceneComponents[0].dialog;
+        //dialogText.GetComponent<TextMeshProUGUI>().text = startLineBlock.sceneComponents[0].dialog;
 
         lineNumber++;
 
+        EndCheck();
 
+
+
+
+    }
+
+    //[SerializeField] TextMeshProUGUI _textMeshPro;
+
+    public string[] stringArray;
+
+    [SerializeField] float timeBtwnChars;
+    [SerializeField] float timeBtwnWords;
+
+    int i = 0;
+
+    public void EndCheck()
+    {
+        Debug.Log("???");
+        if (i <= stringArray.Length - 1)
+        {
+        startLineBlock.sceneComponents[0].dialog = stringArray[i];
+            StartCoroutine(TextVisible());
+
+        Debug.Log("Working");
+        }
+    }
+
+    private IEnumerator TextVisible()
+    {
+        dialogText.GetComponent<TextMeshProUGUI>().ForceMeshUpdate();
+        int totalVisibleCharacters = dialogText.GetComponent<TextMeshProUGUI>().textInfo.characterCount;
+        int counter = 0;
+
+        while (true)
+        {
+            int visibleCount = totalVisibleCharacters + 1;
+            dialogText.GetComponent<TextMeshProUGUI>().maxVisibleCharacters = visibleCount;
+
+            if (visibleCount >= totalVisibleCharacters)
+            {
+                i += 1;
+                Invoke("EndCheck", timeBtwnWords);
+                Debug.Log("Ahhhh");
+                break;
+            }
+
+            counter += 1;
+            yield return new WaitForSeconds(timeBtwnChars);
+
+
+        }
     }
 }
