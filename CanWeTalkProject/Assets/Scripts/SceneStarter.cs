0053b7b152712a31b7ce7e36a2c09a3a800a666d
DELLA\steph <s210449@students.aie.edu.au>
2023-05-04 18:51:08 +0930

    more changes

--- CanWeTalkProject/Assets/Scripts/SceneStarter.cs
+++ CanWeTalkProject/Assets/Scripts/SceneStarter.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class SceneStarter : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class SceneStarter : MonoBehaviour
     public GameObject content;
     public float xPositionSpeechBubble;
     public float yPositionSpeechBubble;
+    public GameObject dialogText;
+    public float lineNumber;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,9 +29,15 @@ public class SceneStarter : MonoBehaviour
     {
         GameObject speechbubble = Instantiate(characterSpeechBubble) as GameObject;
         speechbubble.transform.SetParent(content.transform, false);
+        speechbubble.name = "SpeechBubble" + lineNumber.ToString();
         speechbubble.GetComponent<RectTransform>().anchoredPosition = new Vector2(xPositionSpeechBubble, yPositionSpeechBubble);
 
-        speechbubble.transform.Find("CharacterDialogText");
+
+        dialogText = GameObject.Find("CharacterDialogText");
+
+        dialogText.GetComponent<TextMeshProUGUI>().text = startLineBlock.sceneComponents[0].dialog;
+
+        lineNumber++;
 
 
     }
